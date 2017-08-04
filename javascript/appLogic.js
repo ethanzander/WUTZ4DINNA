315dbb8b610d090b1a715396fcce8a1bb0c704d9
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-08-03 20:43:59 -0600

    testing api js

--- javascript/appLogic.js
+++ javascript/appLogic.js
@@ -1,1 +1,29 @@
-var = oneVar
+
+$('document').ready(function() {
+
+  $("#submit").on("click", function() {
+    event.preventDefault();
+    var ingridient = $("#ingridient1").val().trim();
+
+    var queryURL = "http://food2fork.com/api/search?key=e8a5d977c8d3091746ed1bedca5893b7&q="+ingridient;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+      var results = response.data;
+      console.log(response);
+
+
+
+    });
+
+
+  });
+
+
+
+
+
+});
