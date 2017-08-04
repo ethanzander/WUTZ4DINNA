e716fa747edd970be45c62088eb357a72e3b1887
Ethan Zander <ethanzander13@gmail.com>
2017-08-03 22:48:24 -0600

    added innital firebase server functionality

--- javascript/appLogic.js
+++ javascript/appLogic.js
@@ -1,1 +1,10 @@
-var = oneVar
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBkv1kWVZHfNFQYII8sgOLwX6229OemGUQ",
+    authDomain: "wutz4dinna.firebaseapp.com",
+    databaseURL: "https://wutz4dinna.firebaseio.com",
+    projectId: "wutz4dinna",
+    storageBucket: "",
+    messagingSenderId: "925050261550"
+  };
+  firebase.initializeApp(config);
