2071cf3bf9066b072e77cdc3cdde30fb7d7e6e8f
Ethan Zander <ethanzander13@gmail.com>
2017-08-03 23:52:50 -0600

    Working on Firebase Login

--- javascript/appLogic.js
+++ javascript/appLogic.js
@@ -1,3 +1,9 @@
+$( document ).ready(function(){
+    console.log("Ready!");
+
+
+
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBkv1kWVZHfNFQYII8sgOLwX6229OemGUQ",
@@ -8,3 +14,43 @@
     messagingSenderId: "925050261550"
   };
   firebase.initializeApp(config);
+  //Get Elements
+  const userEmail = document.getElementById('email-input');
+  const userPassword = document.getElementById('user-password');
+  const btnLogin = document.getElementById('btn-login');
+  const btnSignUp = document.getElementById('btn-sign-up');
+  const btnLogout = document.getElementById('btn-logout');
+  //Add login event
+  btnLogin.addEventListener('click', e=> {
+      //Get email and password
+      const email = userEmail.value;
+      const pass = userPassword.value;
+      const auth = firebase.auth();
+      //Sign in
+      const promise = auth.signInWithEmailAndPassword(email, pass);
+      promise.catch(e => console.log(e.message));
+  });
+  //Add signup event
+  btnSignUp.addEventListener('click', e =>{
+    //Get email and password
+    //TODO: Check for real email / validate email
+      const email = userEmail.value;
+      const pass = userPassword.value;
+      const auth = firebase.auth();
+      //Sign in
+      const promise = auth.createUserWithEmailAndPassword(email, pass);
+      promise.catch(e => console.log(e.message));
+  });
+  //Add Realtime listener
+  firebase.auth().onAuthStateChange(firebaseUser => {
+    if (firebaseUser){
+        console.log(firebaseUser);
+        btnLogout.classList.remove("hide");
+    } else {
+        console.log("Not logged in.");
+        btnLogout.classList.add("hide");
+    }
+  });
+
+// End Document.ready
+});
