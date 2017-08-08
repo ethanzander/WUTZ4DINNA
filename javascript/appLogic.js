56375741a8e3ac2ee652c52b7abe3c4aff286e86
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-08-07 20:11:01 -0600

    js

--- javascript/appLogic.js
+++ javascript/appLogic.js
@@ -3,11 +3,11 @@ $('document').ready(function() {
 
   $("#submit").on("click", function() {
     event.preventDefault();
-    var ingridient1 = $("#ingredient1").val().trim();
-    var ingridient2 = $("#ingredient2").val().trim();
-    var ingridient3 = $("#ingredient3").val().trim();
+    var ingredient1 = $("#ingredient1").val();
+    var ingredient2 = $("#ingredient2").val().trim();
+    var ingredient3 = $("#ingredient3").val().trim();
 
-    var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=b21780d2&_app_key=edc2ee3a9551ef7f48b3279d332a2b09&q=" + ingridient1 + "&q=" + ingridient2;
+    var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=b21780d2&_app_key=edc2ee3a9551ef7f48b3279d332a2b09&q=" + ingredient1 + "&q=" + ingredient2 + "&q=" + ingredient3;
 
     $.ajax({
       url: queryURL,
@@ -27,11 +27,14 @@ $('document').ready(function() {
         var recipeDisplay = $('<div class = "recipeDisplay">');
         var image = $('<img class = "image">');
         image.attr("src", response.matches[i].smallImageUrls[0]);
+        image.attr("recipeId", response.matches[i].id);
 
+        var recipeName = $("<p class = 'recipename'>");
+        recipeName.text(response.matches[i].recipeName);
 
 
         recipeDisplay.append(image);
-        recipeDisplay.append(response.matches[i].recipeName);
+        recipeDisplay.append(recipeName);
         console.log(response.matches[i].recipeName);
         $("#recipeDiv").append(recipeDisplay);
 
@@ -40,6 +43,28 @@ $('document').ready(function() {
 
 
 
+
+    });
+
+
+  });
+  $("#clear").on("click", function() {
+    $("#recipeDiv").empty();
+  })
+
+  $(document).on("click", ".image", function() {
+    var recipeId = $(this).attr("recipeId");
+    var queryURL2 = "http://www.yummly.com/recipe/recipe-id?Man-Pleasing-Chicken-608913_app_id=b21780d2&_app_key=edc2ee3a9551ef7f48b3279d332a2b09"
+    $.ajax({
+      url: queryURL2,
+      headers: {
+'Access-Control-Allow-Origin': '*'
+                },
+      method: "GET"
+    })
+    .done(function(response) {
+      console.log(response);
+      console.log(queryURL2);
 
     });
 
