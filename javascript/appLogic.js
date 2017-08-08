7bcd268c0ad17a5608be8f1837c2b78aa5f821f3
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-08-08 12:44:35 -0600

    notes

--- javascript/appLogic.js
+++ javascript/appLogic.js
@@ -8,7 +8,7 @@ $('document').ready(function() {
     var ingredient3 = $("#ingredient3").val().trim();
 
     var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=b21780d2&_app_key=edc2ee3a9551ef7f48b3279d332a2b09&q=" + ingredient1 + "&q=" + ingredient2 + "&q=" + ingredient3;
-
+    //search recepies ajax call
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -16,11 +16,6 @@ $('document').ready(function() {
     .done(function(response) {
       var results = response.matches;
       console.log(response);
-
-
-
-
-
       for (var i = 0; i < results.length; i++) {
 
 
@@ -39,43 +34,28 @@ $('document').ready(function() {
         $("#recipeDiv").append(recipeDisplay);
 
       }
-
-
-
-
-
     });
-
-
   });
+  //clear recipe search
   $("#clear").on("click", function() {
     $("#recipeDiv").empty();
   })
-
+  //code to run when you click on a recipe img
   $(document).on("click", ".image", function() {
     var recipeId = $(this).attr("recipeId");
-    var queryURL2 = "http://www.yummly.com/recipe/recipe-id?Man-Pleasing-Chicken-608913_app_id=b21780d2&_app_key=edc2ee3a9551ef7f48b3279d332a2b09"
+    var queryURL2 = "http://api.yummly.com/v1/api/recipe/" + recipeId + "?_app_id=b21780d2&_app_key=edc2ee3a9551ef7f48b3279d332a2b09"
+
+    //ajax call to get selected recipe info
     $.ajax({
       url: queryURL2,
-      headers: {
-'Access-Control-Allow-Origin': '*'
-                },
       method: "GET"
     })
     .done(function(response) {
+      $("#recipeDiv").empty();
       console.log(response);
-      console.log(queryURL2);
 
     });
 
-
   });
-
-
-
-
-
-
-
 
 });
