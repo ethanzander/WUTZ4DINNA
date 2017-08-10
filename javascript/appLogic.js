bd1b194f487663e0589484e639bcdc5b0c6886a0
Floyd Corbin <floyd.corbin@gmail.com>
2017-08-10 16:09:39 -0600

    Rony's code

--- javascript/appLogic.js
+++ javascript/appLogic.js
@@ -1,6 +1,7 @@
 
 
 $('document').ready(function() {
+  var shoppingList = [];
 
   $("#submit").on("click", function() {
     event.preventDefault();
@@ -22,12 +23,13 @@ $('document').ready(function() {
         var recipeDisplay = $('<div class = "recipeDisplay">');
         var image = $('<img class = "image">');
         image.attr("src", response.matches[i].smallImageUrls[0]);
-        image.attr("recipeId", response.matches[i].id);
+        // image.attr("recipeId", response.matches[i].id);
 
         var recipeName = $("<p class = 'recipename'>");
         recipeName.text(response.matches[i].recipeName);
 
         recipeDisplay.append(image);
+        recipeDisplay.attr('recipeId',response.matches[i].id;
         recipeDisplay.append(recipeName);
         console.log(response.matches[i].recipeName);
         $("#recipeDiv").append(recipeDisplay);
